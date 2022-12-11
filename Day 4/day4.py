f0c9b8c93c5769f83beac4fc1c989b7b211bcb05
Luis Palacios <luis.011695@gmail.com>
2022-12-11 13:26:12 -0600

    D4.p1: complete overlap

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,10 +1,24 @@
 
+def count_overlaping_sections(sections):
+    section1=[int(a) for a in sections[0].split("-")]
+    section2=[int(a) for a in sections[1].split("-")]
+
+    if(section1[0] <= section2[0]):
+        if( section2[1] <= section1[1] ):
+            return 1
+
+    if(section2[0] <= section1[0]):
+        if( section1[1] <= section2[1] ):
+            return 1
+    return 0
+
 
 if __name__ == "__main__":
-    # f = open("input", "r")
-    f = open("test", "r")
+    f = open("input", "r")
+    # f = open("test", "r")
     result=0
     for line in f:
-        result+=0
+        sections=line.split(",")
+        result+=count_overlaping_sections(sections)
     
     print(result)
