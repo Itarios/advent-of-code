064c689f3d035773867d25a3e626735fef8d6ed3
Luis Palacios <luis.011695@gmail.com>
2022-12-11 11:53:50 -0600

    D3.p2: Find comon items groups, changed priority

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -3,21 +3,26 @@ from string import ascii_lowercase, ascii_uppercase, ascii_letters
 # Priority value
 def priority_val(letter):
     if letter not in ascii_letters:
-        raise ValueError
-
+        return 0
     if letter in ascii_lowercase:
         return ord(letter)-96
     return ord(letter)-38
 
 def find_duplicate(line):
     n=len(line)//2
-    return [ priority_val(l) for l in line[:n] if l in line[n:]][0]
+    return [ priority_val(item) for item in line[:n] if item in line[n:]][0]
+
+def find_comon(line1,line2,line3):
+    comon_items=set(line1).intersection(line2).intersection(line3)
+    return sum( [ priority_val(item) for item in comon_items ] )
 
 if __name__ == "__main__":
     f = open("input", "r")
     # f = open("test", "r")
     score=0
-    for line in f:
-        score+=find_duplicate(line)
-    
+    # for line in f:
+    #     score+=find_duplicate(line)
+
+    for line1 in f:
+        score+=find_comon(line1,f.readline(),f.readline())
     print(score)
