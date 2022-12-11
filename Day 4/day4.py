b74866fe94dae2db248bd88119ece5876dc07064
Luis Palacios <luis.011695@gmail.com>
2022-12-11 13:33:17 -0600

    D$.p2: Partial overlap function

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -12,6 +12,13 @@ def count_overlaping_sections(sections):
             return 1
     return 0
 
+def count_partial_overlaping_sections(sections):
+    section1=[int(a) for a in sections[0].split("-")]
+    section2=[int(a) for a in sections[1].split("-")]
+
+    if( section1[1]< section2[0] or section2[1]< section1[0]):
+        return 0
+    return 1
 
 if __name__ == "__main__":
     f = open("input", "r")
@@ -19,6 +26,6 @@ if __name__ == "__main__":
     result=0
     for line in f:
         sections=line.split(",")
-        result+=count_overlaping_sections(sections)
-    
+        # result+=count_overlaping_sections(sections)
+        result+=count_partial_overlaping_sections(sections)
     print(result)
