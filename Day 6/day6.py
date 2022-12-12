332c5e63e1846e6563833655fae97a854bc27aba
Luis Palacios <luis.011695@gmail.com>
2022-12-12 16:17:02 -0600

    D6.p1: find the first 4 all different

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,10 +1,18 @@
 
+def all_different(a,b,c,d):
+    return abs((a-b)*(a-c)*(a-d)*(b-c)*(b-d)*(c-d))
+
+def  start_of_packet_marker(buffer):
+    n=len(buffer)
+    
+    for i in range(4,n):
+        if( all_different(*[ord(c) for c in buffer[i-4:i]]) ):
+            return i
+
 
 if __name__ == "__main__":
-    # f = open("input", "r")
-    f = open("test", "r")
-    result=0
+    f = open("input", "r")
+    # f = open("test", "r")
+
     for line in f:
-        result+=0
-    
-    print(result)
+        print(start_of_packet_marker(line))
