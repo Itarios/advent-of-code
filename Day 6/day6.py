5a9dfb39d91d188a9ef3051f7105e3e7d07039b7
Luis Palacios <luis.011695@gmail.com>
2022-12-12 16:27:42 -0600

    D6.p2: Changed to any number of different values

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -2,6 +2,12 @@
 def all_different(a,b,c,d):
     return abs((a-b)*(a-c)*(a-d)*(b-c)*(b-d)*(c-d))
 
+def all_different_list(lista):
+    while(lista):
+        if( lista.pop() in lista ):
+            return False
+    return True
+
 def  start_of_packet_marker(buffer):
     n=len(buffer)
     
@@ -9,10 +15,16 @@ def  start_of_packet_marker(buffer):
         if( all_different(*[ord(c) for c in buffer[i-4:i]]) ):
             return i
 
+def  start_of_marker(buffer,k):
+    n=len(buffer)
+    
+    for i in range(k,n):
+        if( all_different_list( list(buffer[i-k:i]) ) ):
+            return i
 
 if __name__ == "__main__":
     f = open("input", "r")
     # f = open("test", "r")
 
     for line in f:
-        print(start_of_packet_marker(line))
+        print(start_of_marker(line,14))
