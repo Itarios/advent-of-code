36d44cb522adc638a98577d21e5a90542233a467
Luis Palacios <luis.011695@gmail.com>
2022-12-24 19:27:54 -0600

    D8.p1: debugging flags

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,48 +1,56 @@
-
-
-def count_less_than(num,values):
-    return len( [v for v in values if (v<num)] )
+def count_geq(num,values):
+    geq=[v for v in values if (v>=num)]
+    return len( geq )
 
 def visible_trees(forest):
     n=len(forest)
-    visible=4*(n-1)
+    m=len(forest[0])
+
+    visible=2*(m+n)-4
+    
 
     for i in range(1,n-1):
-        for j in range( 1,n-1 ):
+        
+        for j in range(1,m-1):
             # up=[row[j] for row in forest[:i]]
-            if(count_less_than(forest[i][j], [row[j] for row in forest[:i]] )>0):
+            if(count_geq(forest[i][j], [row[j] for row in forest[:i] ] )==0):
                 visible+=1
                 continue
 
             # down=[row[j] for row in forest[i+1:]]
-            if(count_less_than(forest[i][j], [row[j] for row in forest[i+1:]] )>0):
+            if(count_geq(forest[i][j], [row[j] for row in forest[i+1:] ] )==0):
                 visible+=1
                 continue
 
             # left=forest[i][:j]
-            if(count_less_than(forest[i][j], forest[i][:j] )>0):
+            if(count_geq(forest[i][j], forest[i][:j] )==0):
                 visible+=1
                 continue
 
             # right=forest[i][j+1:]
-            if(count_less_than(forest[i][j], forest[i][j+1:] )>0):
+            if(count_geq(forest[i][j], forest[i][j+1:] )==0):
                 visible+=1
                 continue
 
     return visible
 
-
 if __name__ == "__main__":
-    # f = open("input", "r")
-    f = open("test", "r")
+    f = open("input", "r")
+    # f = open("test", "r")
     result=0
 
     forest=[]
 
     for line in f:
-        forest.append(list(line))
-    f.close()   
-
+        forest.append(list(line)[:-1])
+    f.close()
+    
+    # print(forest)
+    # print( len(forest) )
+    # print( len(forest[0]) )
+    # forest.pop()
+      
+    
     result=visible_trees(forest)
 
     print(result)
