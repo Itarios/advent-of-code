1bd64e7a5891207906ebd2f03c69ef4ccae8e2a3
Luis Palacios <luis.011695@gmail.com>
2022-12-24 19:30:03 -0600

    D8.p2: scenic scores, has some errors

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,3 +1,5 @@
+
+
 def count_geq(num,values):
     geq=[v for v in values if (v>=num)]
     return len( geq )
@@ -34,9 +36,45 @@ def visible_trees(forest):
 
     return visible
 
+
+def scenic_score_direction(tree,direction):
+    n=len(direction)
+
+    for i in range(n):
+        if(direction[i]>=tree):
+            return i+1
+    return n
+    
+def scenic_score(tree, up, down, left, right):
+    return scenic_score_direction(tree, up)*scenic_score_direction(tree, down)*scenic_score_direction(tree, left)*scenic_score_direction(tree, right)
+
+def max_scenic_score(forest):
+    n=len(forest)
+    m=len(forest[0])
+
+    max_ss=1
+    
+
+    for i in range(1,n-1):
+        for j in range(1,m-1):
+            up= reversed([row[j] for row in forest[:i]])
+            
+            down=[row[j] for row in forest[i+1:]]
+            
+            left=reversed(forest[i][:j])
+            
+            right=forest[i][j+1:]
+
+            max_ss= max( max_ss, scenic_score(forest[i][j],up,down,left,right) )
+
+            
+    return scenic_score
+
+
+
 if __name__ == "__main__":
-    f = open("input", "r")
-    # f = open("test", "r")
+    # f = open("input", "r")
+    f = open("test", "r")
     result=0
 
     forest=[]
@@ -45,12 +83,14 @@ if __name__ == "__main__":
         forest.append(list(line)[:-1])
     f.close()
     
-    # print(forest)
-    # print( len(forest) )
+    print(forest)
+    print( len(forest) )
     # print( len(forest[0]) )
-    # forest.pop()
+    forest.pop()
       
     
-    result=visible_trees(forest)
+    # result=visible_trees(forest)
+
+    result=max_scenic_score(forest)
 
     print(result)
