fcf65739d24ac7f0ea1ee5d53bc56dd3aa5f7ec2
Luis Palacios <luis.011695@gmail.com>
2022-12-26 14:28:55 -0600

    D9.p2: added more test cases, expanded the class rope

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -4,13 +4,23 @@ x,y=('x','y')
 
 class Rope:
 
-    H={x:0, y:0}
-    T={x:0, y:0}
-    
-    Tail_visits={(0,0)}
+    def __init__(self, n) -> None:
+        self.H={x:0, y:0}
+        self.Knots=[self.H]
+        self.n=n+1
+        for _ in range(n):
+            self.Knots.append( dict(self.H) )
+        
+        self.T=self.Knots[n]
+
+        self.Tail_visits={ self.knot_to_coordinate(n) }
 
     directions={'R': 1, 'U': 1, 'L': -1, 'D': -1}
     coor={'R': x, 'U': y, 'L': x, 'D': y}
+
+    def knot_to_coordinate(self,knot):
+        return (self.Knots[knot][x], self.Knots[knot][y])
+        
 
     def distance_H_T(self):
         return max( abs(self.diff_x()), abs(self.diff_y()) )
@@ -20,7 +30,7 @@ class Rope:
     
     def diff_y(self):
         return self.H[y]-self.T[y]
-    
+
     def update_T(self) -> None:
         if(self.distance_H_T()<2 ):
             return None
@@ -37,19 +47,51 @@ class Rope:
 
         self.Tail_visits.add( (self.T[x],self.T[y]) )
 
+    
+    def abs_diff_x_knot(self,knot):
+        return abs(self.Knots[knot][x]-self.Knots[knot-1][x])
+
+    def abs_diff_y_knot(self,knot):
+        return abs(self.Knots[knot][y]-self.Knots[knot-1][y])
+    
+    def distance_consecutive_knots(self,knot):
+        return max( self.abs_diff_x_knot(knot),self.abs_diff_y_knot(knot)  )
+
+    def update_knots(self):
+        for knot in range(1,self.n):
+            
+            if( self.distance_consecutive_knots(knot)<2 ):
+                continue
+
+            if(self.Knots[knot-1][x]>self.Knots[knot][x]):
+                self.Knots[knot][x]+=1
+            elif(self.Knots[knot-1][x]<self.Knots[knot][x]):
+                self.Knots[knot][x]-=1
+            
+            if(self.Knots[knot-1][y]>self.Knots[knot][y]):
+                self.Knots[knot][y]+=1
+            elif(self.Knots[knot-1][y]<self.Knots[knot][y]):
+                self.Knots[knot][y]-=1
+
+            self.Tail_visits.add( self.knot_to_coordinate(self.n-1) )
+
     def move_H(self,direction,amount):
+        # print(self.H, direction, amount)
         for _ in range(amount):
             self.H[self.coor[direction]]+=self.directions[direction]
-            self.update_T()
-            # print( f' H:({rope.H[x]},{rope.H[y]}), T:({rope.T[x]},{rope.T[y]}) ')
+            # self.update_T()
+            self.update_knots()
+        
+
 
 
 if __name__ == "__main__":
     f = open("input", "r")
     # f = open("test", "r")
+    # f = open("test2", "r")
     result=0
 
-    rope=Rope()
+    rope=Rope(9)
 
     for line in f:
         direction=line[0]
